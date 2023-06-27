1c7391e0eaf19b2d0598a1694e9140a20adaf369
abdultalif <abdultalif85@gmail.com>
2023-06-27 12:24:37 +0700

    update

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,68 +11,75 @@
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
+    <?php if ($this->session->userdata('role_id') == 1) { ?>
 
+        <div class="sidebar-heading">Admin</div>
+        <li class="nav-item <?= $this->uri->segment(1) == 'admin' ? 'active' : '' ?>">
+            <a class="nav-link" href="<?= base_url('admin'); ?> ">
+                <i class="fas fa-fw fa-user"></i>
+                <span>Role</span>
+            </a>
+        </li>
+
+        <div class="sidebar-heading">user</div>
+        <li class="nav-item <?= $this->uri->segment(1) == 'user' ? 'active' : '' ?>">
+            <a class="nav-link" href="<?= base_url('user'); ?> ">
+                <i class="fas fa-fw fa-user"></i>
+                <span>My Profile</span>
+            </a>
+        </li>
+
+        <hr class="sidebar-divider mt-3">
+        <div class="sidebar-heading">Transaksi</div>
+        <li class="nav-item <?= $this->uri->segment(1) == 'transaksi' ? 'active' : '' ?>">
+            <a class="nav-link" href="<?= base_url('transaksi'); ?> ">
+                <i class="fas fa-fw fa-shopping-cart"></i>
+                <span>Daftar Transaksi</span>
+            </a>
+        </li>
+
+        <hr class="sidebar-divider mt-3">
+        <div class="sidebar-heading">Makanan</div>
+        <li class="nav-item <?= $this->uri->segment(1) == 'makanan' ? 'active' : '' ?>">
+            <a class="nav-link" href="<?= base_url('makanan'); ?> ">
+                <i class="fas fa-fw fa-utensils"></i>
+                <span>Daftar Makanan</span>
+            </a>
+        </li>
+
+    <?php } ?>
+
+
+    <?php if ($this->session->userdata('role_id') == 2) { ?>
+        <hr class="sidebar-divider mt-3">
+        <div class="sidebar-heading">Pesanan</div>
+
+        <!--sub menu looping-->
+        <li class="nav-item <?= $this->uri->segment(1) == 'pesanan' && $this->uri->segment(2) == '' ? 'active' : '' ?>">
+            <a class="nav-link pb-0" href="<?= base_url('pesanan'); ?>">
+                <i class="fas fa-fw fa-shopping-cart"></i>
+                <span>
+                    Pesan Makanan </span></a>
+        </li>
+
+    <?php } ?>
+    <!-- Divider -->
+    <hr class="sidebar-divider mt-3">
+
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('auth/logout'); ?> ">
+            <i class="fas fa-fw fa-sign-out-alt"></i>
+            <span>Logout</span>
+        </a>
+    </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- query menu-->
-    <?php
-    $role_id = $this->session->userdata('role_id');
-    $queryMenu = "SELECT `user_menu`.`id`, `menu`
-                FROM `user_menu` JOIN `user_access_menu`
-                ON `user_menu`.`id` = `user_access_menu`.`menu_id`
-                WHERE `user_access_menu`.`role_id` = $role_id
-                ORDER BY `user_access_menu`.`menu_id` ASC";
-    $menu = $this->db->query($queryMenu)->result_array();
-    ?>
-
-    <!-- looping menu -->
-    <?php foreach ($menu as $m) : ?>
-        <div class="sidebar-heading">
-            <?= $m['menu']; ?>
-        </div>
-
-        <!--sub menu looping-->
-        <?php
-        $menuId = $m['id'];
-        $querySubMenu = "SELECT *
-                            FROM`user_sub_menu` JOIN `user_menu`
-                            ON `user_sub_menu`.`menu_id`=`user_menu`.`id`
-                            WHERE `user_sub_menu`.`menu_id`= $menuId
-                            AND `user_sub_menu`.`is_active`=1";
-        $subMenu = $this->db->query($querySubMenu)->result_array();
-        ?>
-        <?php foreach ($subMenu as $sm) : ?>
-            <?php if ($title == $sm['title']) : ?>
-                <li class="nav-item active">
-                <?php else : ?>
-                <li class="nav-item">
-                <?php endif; ?>
-                <a class="nav-link pb-0" href="<?= base_url($sm['url']); ?> ">
-                    <i class="<?= $sm['icon']; ?>"></i>
-                    <span>
-                        <?= $sm['title']; ?> </span></a>
-                </li>
-            <?php endforeach; ?>
-            <!-- Divider -->
-            <hr class="sidebar-divider mt-3">
-        <?php endforeach; ?>
-
-
-        <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('auth/logout'); ?> ">
-                <i class="fas fa-fw fa-sign-out-alt"></i>
-                <span>Logout</span>
-            </a>
-        </li>
-
-
-
 
 </ul>
 <!-- End of Sidebar -->
