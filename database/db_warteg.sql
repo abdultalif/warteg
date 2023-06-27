a4663fe763e0ea0000dc4a9438da084f7e4f607d
abdultalif <abdultalif85@gmail.com>
2023-06-27 12:26:46 +0700

    update database

--- database/db_warteg.sql
+++ database/db_warteg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 23 Jun 2023 pada 15.52
+-- Waktu pembuatan: 27 Jun 2023 pada 05.26
 -- Versi server: 5.7.33
 -- Versi PHP: 7.4.19
 
@@ -40,7 +40,8 @@ CREATE TABLE `daftar_makanan` (
 
 INSERT INTO `daftar_makanan` (`id`, `nama_makanan`, `harga`, `status`) VALUES
 (1, 'Paket 10.000(bakwan, sayur,nasi)', 10000, 'Tersedia'),
-(3, 'Paket 15.000(ayam, nasi)', 15000, 'Tersedia');
+(3, 'Paket 15.000(ayam, nasi)', 15000, 'Tersedia'),
+(4, 'Paket 20.000(ayam,nasi,sambel)', 20000, 'Tersedia');
 
 -- --------------------------------------------------------
 
@@ -66,8 +67,8 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id`, `email`, `nama_makanan`, `harga_satuan`, `jumlah`, `total`, `status_bayar`, `uang_bayar`, `kembalian`, `tanggal`) VALUES
-(1, 'user@gmail.com', 'Paket 15.000(ayam, nasi)', 15000, 2, 30000, '1', 100000, 70000, '12/12/2020'),
-(4, 'user@gmail.com', 'Paket 10.000(bakwan, sayur,nasi)', 10000, 3, 30000, '0', 0, 0, '13 Dec 2020');
+(5, 'abdultalif85@gmail.com', 'Paket 15.000(ayam, nasi)', 15000, 3, 45000, '1', 50000, 5000, '26 Jun 2023'),
+(6, 'abdultalif85@gmail.com', 'Paket 10.000(bakwan, sayur,nasi)', 10000, 2, 20000, '0', 0, 0, '26 Jun 2023');
 
 -- --------------------------------------------------------
 
@@ -90,67 +91,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`) VALUES
 (21, 'admin', 'admin@gmail.com', 'default.jpg', '$2y$10$.I8kX96a6z0K/Xq8/2.B4ulmAPt7GAD3iqdU8Be3L7iHDqZurYS/.', 1),
-(22, 'user', 'user@gmail.com', 'default.jpg', '$2y$10$.I8kX96a6z0K/Xq8/2.B4ulmAPt7GAD3iqdU8Be3L7iHDqZurYS/.', 2);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user_access_menu`
---
-
-CREATE TABLE `user_access_menu` (
-  `id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `menu_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `user_access_menu`
---
-
-INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(7, 1, 3),
-(8, 1, 22),
-(9, 6, 2),
-(11, 6, 22),
-(12, 6, 23),
-(14, 1, 24),
-(16, 1, 23),
-(17, 1, 25),
-(18, 2, 2),
-(20, 1, 26),
-(21, 3, 2),
-(23, 3, 25),
-(24, 3, 26),
-(26, 2, 26),
-(27, 1, 27),
-(30, 1, 28),
-(31, 2, 29);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user_menu`
---
-
-CREATE TABLE `user_menu` (
-  `id` int(11) NOT NULL,
-  `menu` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `user_menu`
---
-
-INSERT INTO `user_menu` (`id`, `menu`) VALUES
-(1, 'admin'),
-(2, 'user'),
-(3, 'Menu'),
-(27, 'Transaksi'),
-(28, 'Makanan'),
-(29, 'Pesanan');
+(22, 'user', 'user@gmail.com', 'default.jpg', '$2y$10$.I8kX96a6z0K/Xq8/2.B4ulmAPt7GAD3iqdU8Be3L7iHDqZurYS/.', 2),
+(23, 'Abdul Talif Parinduri', 'abdultalif85@gmail.com', 'default.jpg', '$2y$10$01ZqnAEzqq33yPSu/5s/aOBMDf349/iJVMUHu5.HHpB92PM35rprC', 2);
 
 -- --------------------------------------------------------
 
@@ -170,37 +112,6 @@ CREATE TABLE `user_role` (
 INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'administrator'),
 (2, 'user');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user_sub_menu`
---
-
-CREATE TABLE `user_sub_menu` (
-  `id` int(11) NOT NULL,
-  `menu_id` int(11) NOT NULL,
-  `title` varchar(128) NOT NULL,
-  `url` varchar(128) NOT NULL,
-  `icon` varchar(128) NOT NULL,
-  `is_active` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `user_sub_menu`
---
-
-INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
-(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
-(5, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
-(6, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
-(11, 1, 'Role', 'admin', 'fas fa-fw fa-user-tie', 1),
-(19, 24, 'Category Management', 'category', 'fas fa-fw fa-globe', 1),
-(22, 25, 'My Vacancies', 'company', 'fas fa-fw fa-user-md', 1),
-(23, 26, 'All Vacancies', 'Job', 'fas fa-fw fa-globe', 1),
-(26, 28, 'Daftar Makanan', 'makanan', 'fas fa-fw fa-utensils', 1),
-(27, 27, 'Daftar Transaksi', 'transaksi', 'fas fa-fw fa-shopping-cart', 1),
-(29, 29, 'Pesan Makanan', 'pesanan', 'fas fa-fw fa-shopping-cart', 1);
 
 --
 -- Indexes for dumped tables
@@ -225,27 +136,9 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_access_menu`
---
-ALTER TABLE `user_access_menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `user_menu`
---
-ALTER TABLE `user_menu`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indeks untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `user_sub_menu`
---
-ALTER TABLE `user_sub_menu`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -256,43 +149,25 @@ ALTER TABLE `user_sub_menu`
 -- AUTO_INCREMENT untuk tabel `daftar_makanan`
 --
 ALTER TABLE `daftar_makanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT untuk tabel `user_access_menu`
---
-ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
-
---
--- AUTO_INCREMENT untuk tabel `user_menu`
---
-ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `user_sub_menu`
---
-ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
